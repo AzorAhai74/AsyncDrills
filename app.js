3e05f8e1d320ba6a4b63a6e7a705a81a1694686f
Byron Baldwin <byronsbaldwin@gmail.com>
2020-05-26 22:22:35 -0500

    Timing out 1,2, and 3

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 function wordUp(message) {
     console.log(`Hey homie what is the ${message}`);
+    setTimeout(function() {
+        console.log('Hey homie, time waits for no one');
+    }, 2000);
 };
 
 wordUp('word');
+
+
+
+
+
